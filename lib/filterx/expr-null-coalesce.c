02b19f6d8c2597ea9c34dc5d3cd8563dc4aa04a4
shifter <shifter@axoflow.com>
2024-06-15 11:28:37 +0200

    filterx: add null_coalesce operator
    Signed-off-by: shifter <shifter@axoflow.com>

---
+++ lib/filterx/expr-null-coalesce.c
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2024 Axoflow
+ * Copyright (c) 2024 shifter <shifter@axoflow.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "filterx/expr-null-coalesce.h"
+#include "filterx/object-null.h"
+#include "filterx/filterx-eval.h"
+#include "filterx/object-message-value.h"
+
+typedef struct _FilterXNullCoalesce FilterXNullCoalesce;
+
+struct _FilterXNullCoalesce
+{
+  FilterXBinaryOp super;
+};
+
+static FilterXObject *
+_eval(FilterXExpr *s)
+{
+  FilterXNullCoalesce *self = (FilterXNullCoalesce *) s;
+
+  FilterXObject *lhs_object = filterx_expr_eval(self->super.lhs);
+  if (!lhs_object || filterx_object_is_type(lhs_object, &FILTERX_TYPE_NAME(null))
+      || (filterx_object_is_type(lhs_object, &FILTERX_TYPE_NAME(message_value))
+          && filterx_message_value_get_type(lhs_object) == LM_VT_NULL))
+    {
+      if (!lhs_object)
+        {
+          msg_debug("FILTERX null coalesce supressing error:",
+                    filterx_format_last_error());
+          filterx_eval_clear_errors();
+        }
+      FilterXObject *rhs_object = filterx_expr_eval(self->super.rhs);
+      filterx_object_unref(lhs_object);
+      return rhs_object;
+    }
+  return lhs_object;
+}
+
+FilterXExpr *
+filterx_null_coalesce_new(FilterXExpr *lhs, FilterXExpr *rhs)
+{
+  FilterXNullCoalesce *self = g_new0(FilterXNullCoalesce, 1);
+  filterx_binary_op_init_instance(&self->super, lhs, rhs);
+  self->super.super.eval = _eval;
+  return &self->super.super;
+}
