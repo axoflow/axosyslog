385d8f20331dd59c7e1b151b93ae6ff956cf55d0
László Várady <laszlo.varady@anno.io>
2024-09-13 16:43:55 +0200

    filterx-function: type-check generator functions at runtime
    Due to the gpointer plugin trickery, we lose compile-time type information.
    
    Signed-off-by: László Várady <laszlo.varady@anno.io>

--- lib/filterx/expr-function.c
+++ lib/filterx/expr-function.c
@@ -621,7 +621,11 @@ _lookup_generator_function(GlobalConfig *cfg, const gchar *function_name, Filter
 
   if (!ctor)
     return NULL;
-  return ctor(function_name, args, error);
+
+  FilterXExpr *func = ctor(function_name, args, error);
+  g_assert(!func || filterx_expr_is_generator(func));
+
+  return func;
 }
 
 /* NOTE: takes the references of objects passed in "arguments" */
