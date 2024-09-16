bc992d846e1182ac0ab972fa66c3e9d1e0c9d211
shifter <shifter@axoflow.com>
2024-09-17 01:26:57 +0200

    filterx: rework of unset_empties
    - Default empty set: "", null, [], {}.
    - Optional named arguments:
      - recursive: Enables recursive processing of nested dictionaries.
      - ignorecase: Enables case-insensitive matching.
      - replacement: Specifies a filterxobject to replace target elements instead of removing them.
      - targets: A list of elements to identify for removal or replacement, clearing the default empty set.
    - UTF-8 support.
    - ignorecase and recursive args are defaults to TRUE
    
    This change removes elements from the given dictionary or list that match
    the empty set. If the recursive argument is provided, the function will
    process nested dictionaries as well. The replacement argument allows
    replacing target elements with a specified object, and the targets
    argument customizes which elements are removed or replaced, overriding
    the default empty set.
    
    Signed-off-by: shifter <shifter@axoflow.com>

--- lib/filterx/func-unset-empties.c
+++ lib/filterx/func-unset-empties.c
@@ -1,5 +1,7 @@
 /*
+ * Copyright (c) 2023 Axoflow
  * Copyright (c) 2024 Attila Szakacs
+ * Copyright (c) 2024 shifter
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -30,45 +32,90 @@
 #include "filterx/object-list-interface.h"
 #include "filterx/filterx-eval.h"
 #include "filterx/filterx-globals.h"
+#include "filterx/func-flags.h"
+#include "filterx/expr-literal-generator.h"
+#include "filterx/expr-literal.h"
 
-#define FILTERX_FUNC_UNSET_EMPTIES_USAGE "Usage: unset_empties(object, recursive=true)"
+#define FILTERX_FUNC_UNSET_EMPTIES_USAGE "Usage: unset_empties(object, " \
+FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_RECURSIVE"=bool, " \
+FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_IGNORECASE"=bool, " \
+FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_TARGETS"=[objects...], " \
+FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_REPLACEMENT"=str);"
+
+DEFINE_FUNC_FLAGS(FilterXFunctionUnsetEmptiesFlags,
+                  FILTERX_FUNC_UNSET_EMPTIES_FLAG_RECURSIVE,
+                  FILTERX_FUNC_UNSET_EMPTIES_FLAG_IGNORECASE,
+                  FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_NULL,
+                  FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_STRING,
+                  FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_LIST,
+                  FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_DICT
+                 );
 
 typedef struct FilterXFunctionUnsetEmpties_
 {
   FilterXFunction super;
   FilterXExpr *object_expr;
-  gboolean recursive;
+  GPtrArray *targets;
+  FilterXObject *replacement;
+  guint64 flags;
 } FilterXFunctionUnsetEmpties;
 
 static gboolean _process_dict(FilterXFunctionUnsetEmpties *self, FilterXObject *obj);
 static gboolean _process_list(FilterXFunctionUnsetEmpties *self, FilterXObject *obj);
 
+typedef int (*str_cmp_fn)(const char *, const char *);
+
+static gboolean _string_compare(FilterXFunctionUnsetEmpties *self, const gchar *str, str_cmp_fn cmp_fn)
+{
+  guint num_targets = self->targets ? self->targets->len : 0;
+  for (guint i = 0; i < num_targets; i++)
+    {
+      const gchar *target = g_ptr_array_index(self->targets, i);
+      if (cmp_fn(str, target) == 0)
+        return TRUE;
+    }
+  return FALSE;
+}
+
+static gboolean _should_unset_string(FilterXFunctionUnsetEmpties *self, FilterXObject *obj)
+{
+  gsize str_len = 0;
+  const gchar *str = NULL;
+  gchar *casefold_str = NULL;
+  if (!filterx_object_extract_string(obj, &str, &str_len))
+    return FALSE;
+  g_assert(str);
+
+  if (check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_STRING) && (str_len == 0))
+    return TRUE;
+
+  if (check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_IGNORECASE))
+    {
+      casefold_str = g_utf8_casefold(str, str_len);
+      gboolean result = _string_compare(self, casefold_str, g_utf8_collate);
+      g_free(casefold_str);
+      return result;
+    }
+  return _string_compare(self, str, g_strcmp0);
+}
+
 static gboolean
 _should_unset(FilterXFunctionUnsetEmpties *self, FilterXObject *obj)
 {
-  gsize str_len;
-  const gchar *str;
-  if (filterx_object_extract_string(obj, &str, &str_len))
-    {
-      return str_len == 0 ||
-             strcasecmp(str, "n/a") == 0 ||
-             strcmp(str, "-") == 0;
-    }
+  if (check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_NULL) &&
+      filterx_object_is_type(obj, &FILTERX_TYPE_NAME(null)))
+    return TRUE;
 
-  if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(null)))
-    {
-      return TRUE;
-    }
-
-  if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(dict)) ||
-      filterx_object_is_type(obj, &FILTERX_TYPE_NAME(list)))
+  if ((check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_LIST) &&
+       filterx_object_is_type(obj, &FILTERX_TYPE_NAME(list))) ||
+      (check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_DICT) &&
+       filterx_object_is_type(obj, &FILTERX_TYPE_NAME(dict))))
     {
       guint64 len;
       filterx_object_len(obj, &len);
       return len == 0;
     }
-
-  return FALSE;
+  return _should_unset_string(self, obj);
 }
 
 /* Also unsets inner dicts' and lists' values is recursive is set. */
@@ -78,7 +125,7 @@ _add_key_to_unset_list_if_needed(FilterXObject *key, FilterXObject *value, gpoin
   FilterXFunctionUnsetEmpties *self = ((gpointer *) user_data)[0];
   GList **keys_to_unset = ((gpointer *) user_data)[1];
 
-  if (self->recursive)
+  if (check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_RECURSIVE))
     {
       if (filterx_object_is_type(value, &FILTERX_TYPE_NAME(dict)) && !_process_dict(self, value))
         return FALSE;
@@ -103,8 +150,16 @@ _process_dict(FilterXFunctionUnsetEmpties *self, FilterXObject *obj)
   for (GList *elem = keys_to_unset; elem && success; elem = elem->next)
     {
       FilterXObject *key = (FilterXObject *) elem->data;
-      if (!filterx_object_unset_key(obj, key))
-        success = FALSE;
+      if (self->replacement)
+        {
+          if (!filterx_object_set_subscript(obj, key, &self->replacement))
+            success = FALSE;
+        }
+      else
+        {
+          if (!filterx_object_unset_key(obj, key))
+            success = FALSE;
+        }
     }
 
   g_list_free_full(keys_to_unset, (GDestroyNotify) filterx_object_unref);
@@ -132,7 +187,7 @@ _process_list(FilterXFunctionUnsetEmpties *self, FilterXObject *obj)
     {
       FilterXObject *elem = filterx_list_get_subscript(obj, i);
 
-      if (self->recursive)
+      if (check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_RECURSIVE))
         {
           if (filterx_object_is_type(elem, &FILTERX_TYPE_NAME(dict)) && !_process_dict(self, elem))
             {
@@ -148,7 +203,15 @@ _process_list(FilterXFunctionUnsetEmpties *self, FilterXObject *obj)
 
       if (_should_unset(self, elem))
         {
-          if (!filterx_list_unset_index(obj, i))
+          if (self->replacement)
+            {
+              if (!filterx_list_set_subscript(obj, i, &self->replacement))
+                {
+                  filterx_object_unref(elem);
+                  return FALSE;
+                }
+            }
+          else if (!filterx_list_unset_index(obj, i))
             {
               filterx_object_unref(elem);
               return FALSE;
@@ -197,7 +260,10 @@ static void
 _free(FilterXExpr *s)
 {
   FilterXFunctionUnsetEmpties *self = (FilterXFunctionUnsetEmpties *) s;
+  if (self->targets)
+    g_ptr_array_free(self->targets, TRUE);
   filterx_expr_unref(self->object_expr);
+  filterx_object_unref(self->replacement);
   filterx_function_free_method(&self->super);
 }
 
@@ -216,22 +282,171 @@ _extract_object_expr(FilterXFunctionArgs *args, GError **error)
 }
 
 static gboolean
-_extract_optional_args(FilterXFunctionUnsetEmpties *self, FilterXFunctionArgs *args, GError **error)
+_extract_optional_arg_recursive(FilterXFunctionUnsetEmpties *self, FilterXFunctionArgs *args, GError **error)
 {
   gboolean exists, eval_error;
-  gboolean value = filterx_function_args_get_named_literal_boolean(args, "recursive", &exists, &eval_error);
+  gboolean value = filterx_function_args_get_named_literal_boolean(args, FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_RECURSIVE,
+                   &exists, &eval_error);
   if (!exists)
     return TRUE;
 
   if (eval_error)
     {
       g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
-                  "recursive argument must be boolean literal. " FILTERX_FUNC_UNSET_EMPTIES_USAGE);
+                  FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_RECURSIVE" argument must be boolean literal. " FILTERX_FUNC_UNSET_EMPTIES_USAGE);
       return FALSE;
     }
 
-  self->recursive = value;
+  set_flag(&self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_RECURSIVE, value);
+
   return TRUE;
+}
+
+static gboolean
+_extract_optional_arg_ignorecase(FilterXFunctionUnsetEmpties *self, FilterXFunctionArgs *args, GError **error)
+{
+  gboolean exists, eval_error;
+  gboolean value = filterx_function_args_get_named_literal_boolean(args, FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_IGNORECASE,
+                   &exists, &eval_error);
+  if (!exists)
+    return TRUE;
+
+  if (eval_error)
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_IGNORECASE" argument must be boolean literal. " FILTERX_FUNC_UNSET_EMPTIES_USAGE);
+      return FALSE;
+    }
+
+  set_flag(&self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_IGNORECASE, value);
+
+  return TRUE;
+}
+
+
+static gboolean
+_extract_optional_arg_replacement(FilterXFunctionUnsetEmpties *self, FilterXFunctionArgs *args, GError **error)
+{
+  gboolean exists;
+  FilterXObject *value = filterx_function_args_get_named_literal_object(args,
+                         FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_REPLACEMENT, &exists);
+  if (!exists)
+    return TRUE;
+  if (!value)
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_REPLACEMENT" argument must be literal. " FILTERX_FUNC_UNSET_EMPTIES_USAGE);
+      return FALSE;
+    }
+
+  self->replacement = value;
+  return TRUE;
+}
+
+static gboolean
+_handle_target_object(FilterXFunctionUnsetEmpties *self, FilterXObject *target, GError **error)
+{
+  g_assert(target);
+  guint64 len;
+  if (filterx_object_is_type(target, &FILTERX_TYPE_NAME(null)))
+    set_flag(&self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_NULL, TRUE);
+  else if (filterx_object_is_type(target, &FILTERX_TYPE_NAME(list)))
+    {
+      g_assert(filterx_object_len(target, &len));
+      if (len != 0)
+        {
+          g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                      FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_TARGETS" argument list must be empty! " FILTERX_FUNC_UNSET_EMPTIES_USAGE);
+          return FALSE;
+        }
+      set_flag(&self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_LIST, TRUE);
+    }
+  else if (filterx_object_is_type(target, &FILTERX_TYPE_NAME(dict)))
+    {
+      g_assert(filterx_object_len(target, &len));
+      if (len != 0)
+        {
+          g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                      FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_TARGETS" argument dict must be empty! " FILTERX_FUNC_UNSET_EMPTIES_USAGE);
+          return FALSE;
+        }
+      set_flag(&self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_DICT, TRUE);
+    }
+  else if (filterx_object_is_type(target, &FILTERX_TYPE_NAME(string)))
+    {
+      const gchar *str = filterx_string_get_value(target, &len);
+      if (len == 0)
+        {
+          set_flag(&self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_REPLACE_EMPTY_STRING, TRUE);
+          return TRUE;
+        }
+      if (check_flag(self->flags, FILTERX_FUNC_UNSET_EMPTIES_FLAG_IGNORECASE))
+        {
+          g_ptr_array_add(self->targets, g_utf8_casefold(str, len));
+          return TRUE;
+        }
+      g_ptr_array_add(self->targets, g_strdup(str));
+    }
+  else
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_TARGETS" has unknown target type: %s", target->type->name);
+      return FALSE;
+    }
+  return TRUE;
+}
+
+static gboolean
+_target_iterator(FilterXExpr *key, FilterXExpr *value, gpointer user_data)
+{
+  FilterXFunctionUnsetEmpties *self = ((gpointer *) user_data)[0];
+  GError **error = ((gpointer *) user_data)[1];
+
+  if (!filterx_expr_is_literal(value) && !filterx_expr_is_literal_generator(value))
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_TARGETS" list members must be literals!" FILTERX_FUNC_UNSET_EMPTIES_USAGE);
+      return FALSE;
+    }
+
+  FilterXObject *target = filterx_expr_eval(value);
+  g_assert(target);
+
+  gboolean res = _handle_target_object(self, target, error);
+  filterx_object_unref(target);
+  return res;
+}
+
+static gboolean
+_extract_target_objects(FilterXFunctionUnsetEmpties *self, FilterXFunctionArgs *args, GError **error)
+{
+  gboolean exists;
+  FilterXExpr *targets = filterx_function_args_get_named_expr(args,
+                                                              FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_TARGETS);
+  exists = !!targets;
+
+  if (!exists)
+    return TRUE;
+
+  if (!filterx_expr_is_literal_list_generator(targets))
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  FILTERX_FUNC_UNSET_EMPTIES_ARG_NAME_TARGETS" argument must be literal list. " FILTERX_FUNC_UNSET_EMPTIES_USAGE);
+      return FALSE;
+    }
+
+  guint64 num_targets = filterx_expr_literal_generator_len(targets);
+  if (num_targets > 0)
+    reset_flags(&self->flags, self->flags & (FLAG_VAL(FILTERX_FUNC_UNSET_EMPTIES_FLAG_IGNORECASE) |
+                                             FLAG_VAL(FILTERX_FUNC_UNSET_EMPTIES_FLAG_RECURSIVE)));
+  self->targets = g_ptr_array_new_full(num_targets, (GDestroyNotify)g_free);
+
+  gpointer user_data[] = { self, error };
+  gboolean result = TRUE;
+  if (!filterx_literal_dict_generator_foreach(targets, _target_iterator, user_data))
+    result = FALSE;
+  filterx_expr_unref(targets);
+  return result;
 }
 
 static gboolean
@@ -248,8 +463,18 @@ _extract_args(FilterXFunctionUnsetEmpties *self, FilterXFunctionArgs *args, GErr
   if (!self->object_expr)
     return FALSE;
 
-  if (!_extract_optional_args(self, args, error))
+  if (!_extract_optional_arg_recursive(self, args, error))
     return FALSE;
+
+  if (!_extract_optional_arg_ignorecase(self, args, error))
+    return FALSE;
+
+  if (!_extract_optional_arg_replacement(self, args, error))
+    return FALSE;
+
+  if (!_extract_target_objects(self, args, error))
+    return FALSE;
+
 
   return TRUE;
 }
@@ -262,7 +487,7 @@ filterx_function_unset_empties_new(FilterXFunctionArgs *args, GError **error)
   self->super.super.eval = _eval;
   self->super.super.free_fn = _free;
 
-  self->recursive = TRUE;
+  reset_flags(&self->flags, ALL_FLAG_SET(FilterXFunctionUnsetEmptiesFlags));
 
   if (!_extract_args(self, args, error))
     goto error;
