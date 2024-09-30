dd23623c5d4e44a3c046411df81c8a2637fb15e5
Attila Szakacs <attila.szakacs@axoflow.com>
2024-09-30 09:54:41 +0200

    filterx/datetime: fix use after free in strptime()
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

--- lib/filterx/object-datetime.c
+++ lib/filterx/object-datetime.c
@@ -255,10 +255,10 @@ _strptime_eval(FilterXExpr *s)
   const gchar *time_str;
   gsize time_str_len;
   gboolean extract_success = filterx_object_extract_string(time_str_obj, &time_str, &time_str_len);
-  filterx_object_unref(time_str_obj);
 
   if (!extract_success)
     {
+      filterx_object_unref(time_str_obj);
       filterx_eval_push_error("First argument must be string typed. " FILTERX_FUNC_STRPTIME_USAGE, s, NULL);
       return NULL;
     }
@@ -289,6 +289,7 @@ _strptime_eval(FilterXExpr *s)
   else
     result = filterx_null_new();
 
+  filterx_object_unref(time_str_obj);
   return result;
 }
 
