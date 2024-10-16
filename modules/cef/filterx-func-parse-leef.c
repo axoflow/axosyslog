80181d08e3c3c3bb6734f38a598be1a40ed27b6f
shifter <shifter@axoflow.com>
2024-10-16 17:43:11 +0200

    modules/cef/filterx: The `parse_leef` filterx function now supports LEEF 2.0 version.
    Changes include:
    - Updated usage message
    - Enhanced version parsing
    - Added optional 'delimiter' field to parse and configure the LEEF extension's pair separator based on the specified delimiter value.
    
    Signed-off-by: shifter <shifter@axoflow.com>

--- modules/cef/filterx-func-parse-leef.c
+++ modules/cef/filterx-func-parse-leef.c
@@ -22,19 +22,105 @@
  *
  */
 
+#include <errno.h>
+#include <ctype.h>
+
 #include "filterx-func-parse-leef.h"
 #include "event-format-parser.h"
 
+#include "filterx/object-string.h"
+
 #include "scanner/csv-scanner/csv-scanner.h"
 #include "scanner/kv-scanner/kv-scanner.h"
+#include "filterx/func-flags.h"
+
+DEFINE_FUNC_FLAGS(FilterXFunctionParseLeefFlags,
+                  FILTERX_FUNC_PARSE_LEEF_FLAG_20
+                 );
+
+static gboolean
+_parse_hex_delimiter(const gchar *hexStr, gchar *delimiter)
+{
+  errno = 0;
+  *delimiter = (gchar)strtol(hexStr, NULL, 16);
+  return (errno == 0);
+}
+
+static gboolean
+_delimiter_multi_parser(const gchar *input, gint input_len, gchar *delimiter, GError **error)
+{
+  const gchar *hexStr = NULL;
+  switch (input_len)
+    {
+    case 0:
+      return TRUE; // do not change
+    case 1:
+      *delimiter = input[0];
+      return TRUE;
+    case 3:
+      if (input[0] == 'x' || input[0] == 'X')
+        hexStr = &input[1];
+      break;
+    case 4:
+      if (input[0] == '0' && (input[1] == 'x' || input[1] == 'X'))
+        hexStr = &input[2];
+      break;
+    default:
+      return FALSE; // no match
+    }
+  if (!hexStr)
+    return FALSE;
+  return _parse_hex_delimiter(hexStr, delimiter);
+}
+
+static gboolean
+_is_pair_separator_forced(EventParserContext *ctx)
+{
+  return ctx->parser->kv_pair_separator != NULL;
+}
+
+static gboolean
+_is_delmiter_empty(gchar delimiter)
+{
+  return delimiter == 0;
+}
+
+FilterXObject *
+parse_delimiter(EventParserContext *ctx, const gchar *input, gint input_len, GError **error, gpointer user_data)
+{
+  if (!check_flag(ctx->flags, FILTERX_FUNC_PARSE_LEEF_FLAG_20))
+    return NULL;
+  gchar delimiter = 0;
+  if (_delimiter_multi_parser(input, input_len, &delimiter, error))
+    {
+      if (_is_delmiter_empty(delimiter))
+        return filterx_string_new("", 0);
+      if (!_is_pair_separator_forced(ctx))
+        {
+          ctx->kv_parser_pair_separator[0] = delimiter;
+          ctx->kv_parser_pair_separator[1] = 0;
+        }
+      return filterx_string_new(&delimiter, 1);
+    }
+  return NULL;
+}
+
+FilterXObject *
+parse_leef_version(EventParserContext *ctx, const gchar *value, gint value_len, GError **error, gpointer user_data)
+{
+  if (g_strstr_len(value, value_len, "2.0"))
+    set_flag(&ctx->flags, FILTERX_FUNC_PARSE_LEEF_FLAG_20, TRUE);
+  return parse_version(ctx, value, value_len, error, user_data); // call base class parser
+}
 
 static Field leef_fields[] =
 {
-  { .name = "version", .field_parser = parse_version},
+  { .name = "version", .field_parser = parse_leef_version},
   { .name = "vendor"},
   { .name = "product_name"},
   { .name = "product_version"},
   { .name = "event_id"},
+  { .name = "delimiter", .optional=TRUE, .field_parser = parse_delimiter},
   { .name = "extensions", .field_parser = parse_extensions},
 };
 
@@ -53,7 +139,7 @@ filterx_function_parse_leef_new(FilterXFunctionArgs *args, GError **err)
   {
     .signature = "LEEF",
     .header = {
-      .num_fields = 6,
+      .num_fields = 7,
       .delimiters = "|",
       .fields = leef_fields,
     },
