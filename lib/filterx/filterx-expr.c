01ca1b41b11d1fde6db82682c705c9d7af1ca13c
Balazs Scheidler <balazs.scheidler@axoflow.com>
2024-12-28 20:43:57 +0100

    filterx/filterx-expr: add expr level detail to perf stacktraces
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- lib/filterx/filterx-expr.c
+++ lib/filterx/filterx-expr.c
@@ -28,6 +28,13 @@
 #include "mainloop.h"
 #include "stats/stats-registry.h"
 #include "stats/stats-cluster-single.h"
+#include "perf/perf.h"
+
+static inline gboolean
+_extract_source_text(void)
+{
+  return debug_flag || perf_is_enabled();
+}
 
 void
 filterx_expr_set_location_with_text(FilterXExpr *self, CFG_LTYPE *lloc, const gchar *text)
@@ -36,7 +43,7 @@ filterx_expr_set_location_with_text(FilterXExpr *self, CFG_LTYPE *lloc, const gc
     self->lloc = g_new0(CFG_LTYPE, 1);
   *self->lloc = *lloc;
 
-  if (debug_flag && text && text != self->expr_text)
+  if (_extract_source_text() && text && text != self->expr_text)
     {
       g_free(self->expr_text);
       self->expr_text = g_strdup(text);
@@ -49,7 +56,7 @@ filterx_expr_set_location(FilterXExpr *self, CfgLexer *lexer, CFG_LTYPE *lloc)
   if (!self->lloc)
     self->lloc = g_new0(CFG_LTYPE, 1);
   *self->lloc = *lloc;
-  if (debug_flag)
+  if (_extract_source_text())
     {
       g_free(self->expr_text);
       GString *res = g_string_sized_new(0);
@@ -114,7 +121,7 @@ _init_sc_key_name(FilterXExpr *self, gchar *buf, gsize buf_len)
 gboolean
 filterx_expr_init_method(FilterXExpr *self, GlobalConfig *cfg)
 {
-  gchar buf[64];
+  gchar buf[256];
 
   _init_sc_key_name(self, buf, sizeof(buf));
   stats_lock();
@@ -127,6 +134,16 @@ filterx_expr_init_method(FilterXExpr *self, GlobalConfig *cfg)
   stats_cluster_single_key_set(&sc_key, buf, labels, labels_len);
   stats_register_counter(STATS_LEVEL3, &sc_key, SC_TYPE_SINGLE_VALUE, &self->eval_count);
   stats_unlock();
+
+  if (perf_is_enabled())
+    {
+      if (self->expr_text)
+        g_snprintf(buf, sizeof(buf), "filterx::%s", self->expr_text);
+      else
+        g_snprintf(buf, sizeof(buf), "filterx::@%s", self->type);
+      self->eval = perf_generate_trampoline(self->eval, buf);
+    }
+
   return TRUE;
 }
 
