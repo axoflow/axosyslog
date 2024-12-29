10beaf4b03ff73593f2d5d25e22820514a54b1c4
Balazs Scheidler <balazs.scheidler@axoflow.com>
2024-12-30 00:01:33 +0100

    templates: make log_template_get_global_template_options() inline
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- lib/template/globals.c
+++ lib/template/globals.c
@@ -25,13 +25,7 @@
 #include "templates.h"
 #include "macros.h"
 
-static LogTemplateOptions global_template_options;
-
-LogTemplateOptions *
-log_template_get_global_template_options(void)
-{
-  return &global_template_options;
-}
+LogTemplateOptions global_template_options;
 
 void
 log_template_global_init(void)
