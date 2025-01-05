d27ba82c96f32faa4c8fd10c30aabf2bb89bc04e
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-01-05 16:20:53 +0100

    filterx-eval: extract context begin/end code as macros
    Although this code has a single user, it is relatively complex, relies
    on arcane mechanics of FilterXScope and FilterXEvalContext. Hide it
    and delegate it to show where it belongs.
    
    These have become macros, as we need to use the caller's stack frame to
    allocate the scope.
    
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- lib/filterx/filterx-eval.h
+++ lib/filterx/filterx-eval.h
@@ -127,5 +127,30 @@ filterx_eval_store_weak_ref(FilterXObject *object)
     }
 }
 
+#define FILTERX_EVAL_BEGIN_CONTEXT(eval_context, previous_context) \
+  do { \
+    FilterXScope *scope = NULL; \
+    gboolean local_scope = FALSE; \
+    \
+    if (previous_context) \
+      scope = filterx_scope_reuse(previous_context->scope); \
+    \
+    if (!scope) \
+      { \
+        gsize alloc_size = filterx_scope_get_alloc_size(); \
+        scope = g_alloca(alloc_size); \
+        filterx_scope_init_instance(scope, alloc_size, path_options->filterx_context ? path_options->filterx_context->scope : NULL); \
+        local_scope = TRUE; \
+      } \
+    filterx_eval_init_context(&eval_context, path_options->filterx_context, scope, msg); \
+    do
+
+
+#define FILTERX_EVAL_END_CONTEXT(eval_context) \
+    while(0); \
+    filterx_eval_deinit_context(&eval_context); \
+    if (local_scope) \
+      filterx_scope_clear(scope); \
+  } while(0)
 
 #endif
