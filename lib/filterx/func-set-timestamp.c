8971b1452b73ec8564e3c3273fc0a3699daeff9b
Tamás Kosztyu <tamas.kosztyu@axoflow.com>
2025-02-14 14:17:58 +0100

    filterx/func-set-timestamp: add set_timestamp() arg parsing
    Signed-off-by: Tamás Kosztyu <tamas.kosztyu@axoflow.com>

--- lib/filterx/func-set-timestamp.c
+++ lib/filterx/func-set-timestamp.c
@@ -23,10 +23,19 @@
  */
 
 #include "func-set-timestamp.h"
+#include "object-datetime.h"
+#include "filterx/object-extractor.h"
+#include "filterx/object-primitive.h"
+#include "filterx/filterx-eval.h"
+
+#define FILTERX_FUNC_SET_TIMESTAMP_USAGE "Usage: set_timestamp(datetime, stamp=[\"stamp\", \"recvd\"])"
 
 typedef struct FilterXFunctionSetTimestamp_
 {
   FilterXFunction super;
+
+  FilterXExpr *datetime_expr;
+  LogMessageTimeStamp timestamp_idx;
 } FilterXFunctionSetTimestamp;
 
 static FilterXObject *
@@ -40,13 +49,75 @@ _set_timestamp_free(FilterXExpr *s)
 {
   FilterXFunctionSetTimestamp *self = (FilterXFunctionSetTimestamp *) s;
 
+  filterx_expr_unref(self->datetime_expr);
   filterx_function_free_method(&self->super);
 }
 
+static const gchar *
+_extract_set_timestamp_idx_str(FilterXFunctionArgs *args, GError **error)
+{
+  gboolean exists;
+  const gchar *idx_str = filterx_function_args_get_named_literal_string(args, "stamp", NULL, &exists);
+
+  if (!exists)
+    {
+      return "stamp";
+    }
+
+  return idx_str;
+}
+
+static FilterXExpr *
+_extract_set_timestamp_datetime_expr(FilterXFunctionArgs *args, GError **error)
+{
+  FilterXExpr *datetime_expr = filterx_function_args_get_expr(args, 0);
+  if (!datetime_expr)
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  "argument must be set: datetime. " FILTERX_FUNC_SET_TIMESTAMP_USAGE);
+      return NULL;
+    }
+
+  return datetime_expr;
+}
 
 static gboolean
 _extract_set_timestamp_args(FilterXFunctionSetTimestamp *self, FilterXFunctionArgs *args, GError **error)
 {
+  gsize len = filterx_function_args_len(args);
+
+  if (len != 1 && len != 2)
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  "invalid number of arguments. " FILTERX_FUNC_SET_TIMESTAMP_USAGE);
+      return FALSE;
+    }
+
+  self->datetime_expr = _extract_set_timestamp_datetime_expr(args, error);
+  if (!self->datetime_expr)
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  "First argument must be of datetime type. " FILTERX_FUNC_SET_TIMESTAMP_USAGE);
+      return FALSE;
+    }
+
+  const gchar *idx_str = _extract_set_timestamp_idx_str(args, error);
+  if (!idx_str)
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  "Second argument must be string type. " FILTERX_FUNC_SET_TIMESTAMP_USAGE);
+      return FALSE;
+    }
+
+  self->timestamp_idx = log_msg_lookup_time_stamp_name(idx_str);
+
+  if ((self->timestamp_idx != LM_TS_STAMP) && (self->timestamp_idx != LM_TS_RECVD))
+    {
+      g_set_error(error, FILTERX_FUNCTION_ERROR, FILTERX_FUNCTION_ERROR_CTOR_FAIL,
+                  "Invalid timestamp type " FILTERX_FUNC_SET_TIMESTAMP_USAGE);
+      return FALSE;
+    }
+
   return TRUE;
 }
 
