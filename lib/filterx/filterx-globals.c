ca297811b3f08ff70067cde5776e176bb023a0ee
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-02-14 14:09:53 +0100

    filterx: move cache_json_file() implementation to the core
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- lib/filterx/filterx-globals.c
+++ lib/filterx/filterx-globals.c
@@ -45,6 +45,7 @@
 #include "filterx/func-flatten.h"
 #include "filterx/func-sdata.h"
 #include "filterx/func-repr.h"
+#include "filterx/func-cache-json-file.h"
 #include "filterx/expr-regexp-search.h"
 #include "filterx/expr-regexp-subst.h"
 #include "filterx/expr-regexp.h"
@@ -163,6 +164,7 @@ _ctors_init(void)
   g_assert(filterx_builtin_function_ctor_register("strftime", filterx_function_strftime_new));
   g_assert(filterx_builtin_function_ctor_register("keys", filterx_function_keys_new));
   g_assert(filterx_builtin_function_ctor_register("vars", filterx_function_vars_new));
+  g_assert(filterx_builtin_function_ctor_register("cache_json_file", filterx_function_cache_json_file_new));
 }
 
 static void
