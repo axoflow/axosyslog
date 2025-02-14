f50bcea780ce4022a9aaf1889e65d48a67913834
László Várady <laszlo.varady@anno.io>
2025-02-14 16:05:54 +0100

    filterx-cache-json: make subobject readonly
    To avoid write-related races (_json_string() too) when the object is
    shared across threads.
    
    Signed-off-by: László Várady <laszlo.varady@anno.io>

--- modules/json/filterx-cache-json-file.c
+++ modules/json/filterx-cache-json-file.c
@@ -205,6 +205,8 @@ _deep_freeze(FilterXFuntionCacheJsonFile *self, FilterXObject *object)
   if (filterx_object_freeze(object))
     g_ptr_array_add(self->frozen_objects, object);
 
+  filterx_object_make_readonly(object);
+
   object = filterx_ref_unwrap_ro(object);
   if (filterx_object_is_type(object, &FILTERX_TYPE_NAME(json_object)))
     _deep_freeze_dict(self, object);
