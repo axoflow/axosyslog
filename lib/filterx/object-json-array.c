2d8ff42ccc933443e87cc092bd3ab7b525b0655c
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-02-14 17:27:31 +0100

    filterx/filterx-ref: add dirty tracking to FilterXRef
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- lib/filterx/object-json-array.c
+++ lib/filterx/object-json-array.c
@@ -26,7 +26,6 @@
 #include "filterx/object-primitive.h"
 #include "filterx/object-string.h"
 #include "filterx/object-message-value.h"
-#include "filterx/filterx-weakrefs.h"
 #include "filterx/object-list-interface.h"
 #include "filterx/expr-function.h"
 #include "filterx/filterx-eval.h"
@@ -163,11 +162,11 @@ _append(FilterXList *s, FilterXObject **new_value)
       return FALSE;
     }
 
-  filterx_object_set_modified_in_place(&self->super.super, TRUE);
+  filterx_object_set_dirty(&self->super.super, TRUE);
   FilterXObject *root_container = filterx_weakref_get(&self->root_container);
   if (root_container)
     {
-      filterx_object_set_modified_in_place(root_container, TRUE);
+      filterx_object_set_dirty(root_container, TRUE);
       filterx_object_unref(root_container);
     }
 
@@ -197,11 +196,11 @@ _set_subscript(FilterXList *s, guint64 index, FilterXObject **new_value)
       return FALSE;
     }
 
-  filterx_object_set_modified_in_place(&self->super.super, TRUE);
+  filterx_object_set_dirty(&self->super.super, TRUE);
   FilterXObject *root_container = filterx_weakref_get(&self->root_container);
   if (root_container)
     {
-      filterx_object_set_modified_in_place(root_container, TRUE);
+      filterx_object_set_dirty(root_container, TRUE);
       filterx_object_unref(root_container);
     }
 
@@ -222,11 +221,11 @@ _unset_index(FilterXList *s, guint64 index)
   if (json_object_array_del_idx(self->jso, index, 1) != 0)
     return FALSE;
 
-  filterx_object_set_modified_in_place(&self->super.super, TRUE);
+  filterx_object_set_dirty(&self->super.super, TRUE);
   FilterXObject *root_container = filterx_weakref_get(&self->root_container);
   if (root_container)
     {
-      filterx_object_set_modified_in_place(root_container, TRUE);
+      filterx_object_set_dirty(root_container, TRUE);
       filterx_object_unref(root_container);
     }
 
