68d20ed0d9a957b5cfe1876391303fa4704eadbf
Tamás Kosztyu <tamas.kosztyu@axoflow.com>
2025-02-21 09:56:24 +0100

    filterx/func-set-timestamp: use optimize() method
    Signed-off-by: Tamás Kosztyu <tamas.kosztyu@axoflow.com>

--- lib/filterx/func-set-timestamp.c
+++ lib/filterx/func-set-timestamp.c
@@ -69,6 +69,15 @@ _set_timestamp_eval(FilterXExpr *s)
   return filterx_boolean_new(TRUE);
 }
 
+static FilterXExpr *
+_set_timestamp_optimize(FilterXExpr *s)
+{
+  FilterXFunctionSetTimestamp *self = (FilterXFunctionSetTimestamp *) s;
+
+  self->datetime_expr = filterx_expr_optimize(self->datetime_expr);
+  return filterx_function_optimize_method(&self->super);
+}
+
 static gboolean
 _set_timestamp_init(FilterXExpr *s, GlobalConfig *cfg)
 {
@@ -174,6 +183,7 @@ filterx_function_set_timestamp_new(FilterXFunctionArgs *args, GError **error)
   filterx_function_init_instance(&self->super, "set_timestamp");
 
   self->super.super.eval = _set_timestamp_eval;
+  self->super.super.optimize = _set_timestamp_optimize;
   self->super.super.init = _set_timestamp_init;
   self->super.super.deinit = _set_timestamp_deinit;
   self->super.super.free_fn = _set_timestamp_free;
