0c25387f3df9e4999856e1e65457e3404f20fa50
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-04-05 12:41:13 +0200

    light: move test_unset_empties() to test_filterx_funcs
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- tests/light/functional_tests/filterx/test_filterx_funcs.py
+++ tests/light/functional_tests/filterx/test_filterx_funcs.py
@@ -274,3 +274,75 @@ def test_startswith_endswith_includes(config, syslog_ng):
 
     assert file_final.get_stats()["processed"] == 1
     assert file_final.read_log() == '{"startswith_foo":true,"contains_bar":true,"endswith_baz":true,"works_with_message_value":true}'
+
+
+def test_unset_empties(config, syslog_ng):
+    (file_final,) = create_config(
+        config, r"""
+            dict = {"foo": "", "bar": "-", "baz": "N/A", "almafa": null, "kortefa": {"a":{"s":{"d":{}}}}, "szilvafa": [[[]]]};
+            defaults_dict = dict;
+            explicit_dict = dict;
+            no_defaults_dict = dict;
+            target_dict = dict;
+            ignorecase_dict = dict;
+            replacement_dict = dict;
+            unset_empties(defaults_dict, recursive=false);
+            unset_empties(explicit_dict, recursive=true);
+            unset_empties(no_defaults_dict, targets=["n/a", "-"], recursive=true, ignorecase=true);
+            unset_empties(target_dict, targets=["n/a", "-", null, "", {}, []], recursive=true, ignorecase=false);
+            unset_empties(ignorecase_dict, targets=["n/a", "-", null, "", {}, []], recursive=true, ignorecase=true);
+            unset_empties(replacement_dict, targets=["n/a", "-", null, "", {}, []], recursive=true, ignorecase=true, replacement="do");
+
+            list = ["", "-", "N/A", null, {"a":{"s":{"d":{}}}}, [[[]]]];
+            defaults_list = list;
+            explicit_list = list;
+            no_defaults_list = list;
+            target_list = list;
+            ignorecase_list = list;
+            replacement_list = list;
+            unset_empties(defaults_list, recursive=false);
+            unset_empties(explicit_list, recursive=true);
+            unset_empties(no_defaults_list, targets=["n/a", "-"], recursive=true, ignorecase=true);
+            unset_empties(target_list, targets=["n/a", "-", null, "", {}, []], recursive=true, ignorecase=false);
+            unset_empties(ignorecase_list, targets=["n/a", "-", null, "", {}, []], recursive=true, ignorecase=true);
+            unset_empties(replacement_list, targets=["n/a", "-", null, "", {}, []], recursive=true, ignorecase=true, replacement="do");
+
+            $MSG = [
+                    defaults_dict,
+                    explicit_dict,
+                    no_defaults_dict,
+                    target_dict,
+                    ignorecase_dict,
+                    replacement_dict,
+                    defaults_list,
+                    explicit_list,
+                    no_defaults_list,
+                    target_list,
+                    ignorecase_list,
+                    replacement_list
+                    ];
+    """,
+    )
+    syslog_ng.start(config)
+
+    assert file_final.get_stats()["processed"] == 1
+
+    exp = (
+        r"""["""
+        r"""{"bar":"-","baz":"N/A","kortefa":{"a":{"s":{"d":{}}}},"szilvafa":[[[]]]}"""
+        r""",{"bar":"-","baz":"N/A"}"""
+        r""",{"foo":"","almafa":null,"kortefa":{"a":{"s":{"d":{}}}},"szilvafa":[[[]]]}"""
+        r""",{"baz":"N/A"}"""
+        r""",{}"""
+        r""",{"foo":"do","bar":"do","baz":"do","almafa":"do","kortefa":{"a":{"s":{"d":"do"}}},"szilvafa":[["do"]]}"""
+        r""",["-","N/A",{"a":{"s":{"d":{}}}},[[[]]]]"""
+        r""",["-","N/A"]"""
+        r""",["",null,{"a":{"s":{"d":{}}}},[[[]]]]"""
+        r""",["N/A"]"""
+        r""",[]"""
+        r""",["do","do","do","do",{"a":{"s":{"d":"do"}}},[["do"]]]"""
+        r"""]"""
+        """"""
+    )
+
+    assert file_final.read_log() == exp
