0400e8c37931d350773bbba7da583637751914c7
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-04-28 21:58:10 +0200

    mege: expr-list

--- lib/filterx/filterx-expr-list.h
+++ lib/filterx/filterx-expr-list.h
@@ -27,6 +27,7 @@ typedef struct _FilterXPointerList
   gboolean is_sealed;
 } FilterXPointerList;
 
+gboolean filterx_pointer_list_foreach_ref(FilterXPointerList *self, FilterXPointerListForeachRefFunc func, gpointer user_data);
 gboolean filterx_pointer_list_foreach(FilterXPointerList *self, FilterXPointerListForeachFunc func, gpointer user_data);
 
 void filterx_pointer_list_init(FilterXPointerList *self);
@@ -53,6 +54,27 @@ filterx_pointer_list_add(FilterXPointerList *self, gpointer value)
   g_ptr_array_add(self->mut.pointers, value);
 }
 
+static inline gpointer
+filterx_pointer_list_index(FilterXPointerList *self, gsize index)
+{
+  if (self->is_sealed)
+    {
+      g_assert(index < self->sealed.pointers_len);
+      return self->sealed.pointers[index];
+    }
+  else
+    return g_ptr_array_index(self->mut.pointers, index);
+}
+
+static inline gpointer
+filterx_pointer_list_index_fast(FilterXPointerList *self, gsize index)
+{
+#if SYSLOG_NG_ENABLE_DEBUG
+  g_assert(self->is_sealed == TRUE);
+#endif
+  return self->sealed.pointers[index];
+}
+
 static inline gsize
 filterx_pointer_list_get_length(FilterXPointerList *self)
 {
@@ -60,6 +82,65 @@ filterx_pointer_list_get_length(FilterXPointerList *self)
     return self->sealed.pointers_len;
   else
     return self->mut.pointers->len;
+}
+
+typedef gboolean (*FilterXExprListForeachRefFunc)(FilterXExpr **pvalue, gpointer user_data);
+typedef gboolean (*FilterXExprListForeachFunc)(FilterXExpr *value, gpointer user_data);
+typedef FilterXPointerList FilterXExprList;
+
+static inline gboolean
+filterx_expr_list_foreach_ref(FilterXExprList *self, FilterXExprListForeachRefFunc func, gpointer user_data)
+{
+  return filterx_pointer_list_foreach_ref(self, (FilterXPointerListForeachRefFunc) func, user_data);
+}
+
+static inline gboolean
+filterx_expr_list_foreach(FilterXExprList *self, FilterXExprListForeachFunc func, gpointer user_data)
+{
+  return filterx_pointer_list_foreach(self, (FilterXPointerListForeachFunc) func, user_data);
+}
+
+static inline void
+filterx_expr_list_seal(FilterXExprList *self)
+{
+  filterx_pointer_list_seal(self);
+}
+
+static inline void
+filterx_expr_list_add(FilterXExprList *self, FilterXExpr *expr)
+{
+  filterx_pointer_list_add(self, expr);
+}
+
+static inline FilterXExpr *
+filterx_expr_list_index(FilterXExprList *self, gsize index)
+{
+  return (FilterXExpr *) filterx_pointer_list_index(self, index);
+}
+
+static inline FilterXExpr *
+filterx_expr_list_index_fast(FilterXExprList *self, gsize index)
+{
+  return (FilterXExpr *) filterx_pointer_list_index_fast(self, index);
+}
+
+static inline gsize
+filterx_expr_list_get_length(FilterXExprList *self)
+{
+  return filterx_pointer_list_get_length(self);
+}
+
+
+static inline void
+filterx_expr_list_init(FilterXExprList *self)
+{
+  filterx_pointer_list_init(self);
+}
+
+static inline void
+filterx_expr_list_clear(FilterXExprList *self)
+{
+  filterx_pointer_list_clear(self, (GDestroyNotify) filterx_expr_unref);
 }
 
 #endif
