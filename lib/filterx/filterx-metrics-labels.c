9a08c02a5fd7786eb7520737e56b80c2ccac7cd0
Attila Szakacs <attila.szakacs@axoflow.com>
2025-06-04 09:05:57 +0200

    filterx: error info rewording
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

--- lib/filterx/filterx-metrics-labels.c
+++ lib/filterx/filterx-metrics-labels.c
@@ -90,7 +90,7 @@ _format_value_expr(FilterXExpr *expr, gchar **formatted_value)
   if (!(*formatted_value))
     {
       success = FALSE;
-      filterx_eval_push_error("failed to format metrics label value, str() failed", expr, obj);
+      filterx_eval_push_error("Failed to format metrics label value: str() failed", expr, obj);
       goto exit;
     }
 
@@ -189,7 +189,8 @@ _init_label_name(FilterXExpr *name)
 {
   if (!filterx_expr_is_literal(name))
     {
-      filterx_eval_push_error("failed to initialize metrics label name, name must be a string literal", name, NULL);
+      filterx_eval_push_error_info("Failed to initialize metrics label name", name,
+                                   "Name must be a string literal, got an expression", FALSE);
       return NULL;
     }
 
