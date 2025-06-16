f409f37b226c87829a232e4d3e5df5d44391ab18
Attila Szakacs <attila.szakacs@axoflow.com>
2025-06-16 18:24:02 +0200

    modules/grpc: move ProtoSchemaProvider to separate file
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

--- modules/grpc/common/schema/grpc-schema.hpp
+++ modules/grpc/common/schema/grpc-schema.hpp
@@ -32,11 +32,8 @@
 #include "logpipe.h"
 #include "compat/cpp-end.h"
 
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/dynamic_message.h>
-#include <google/protobuf/message.h>
-#include <google/protobuf/compiler/importer.h>
+#include "proto-schema-provider.hpp"
+
 #include <grpc++/grpc++.h>
 
 #include <memory>
@@ -91,77 +88,6 @@ struct Field
     return *this;
   }
 
-};
-
-class ProtoSchemaProvider
-{
-public:
-  virtual bool init() = 0;
-
-  const google::protobuf::Message &get_schema_prototype() const
-  {
-    return *this->schema_prototype;
-  }
-
-  const google::protobuf::Descriptor &get_schema_descriptor() const
-  {
-    return *this->schema_descriptor;
-  }
-
-protected:
-  const google::protobuf::Descriptor *schema_descriptor = nullptr;
-  const google::protobuf::Message *schema_prototype = nullptr;
-};
-
-class ProtoSchemaFileLoader : public ProtoSchemaProvider
-{
-public:
-  ProtoSchemaFileLoader() {};
-
-  bool init();
-
-  void set_proto_file_path(const std::string &proto_file_path_)
-  {
-    this->proto_file_path = proto_file_path_;
-  }
-
-private:
-  bool loaded = false;
-  std::string proto_file_path;
-  std::unique_ptr<google::protobuf::compiler::Importer> importer = nullptr;
-
-  /* A given descriptor_pool/importer instance should outlive msg_factory, as msg_factory caches prototypes */
-  std::unique_ptr<google::protobuf::DynamicMessageFactory> msg_factory = nullptr;
-
-  std::unique_ptr<google::protobuf::compiler::DiskSourceTree> src_tree = nullptr;
-  std::unique_ptr<google::protobuf::compiler::MultiFileErrorCollector> error_coll = nullptr;
-};
-
-class ProtoSchemaBuilder: public ProtoSchemaProvider
-{
-public:
-  using MapTypeFn =
-    std::function<bool (const std::string &type_in, google::protobuf::FieldDescriptorProto::Type &type_out)>;
-
-public:
-  ProtoSchemaBuilder(MapTypeFn map_type, int proto_version, const std::string &file_descriptor_proto_name,
-                     const std::string &descriptor_proto_name);
-
-  bool init();
-
-  bool add_field(const std::string &name, const std::string &type);
-
-private:
-  MapTypeFn map_type;
-  google::protobuf::DescriptorPool descriptor_pool;
-
-  /* A given descriptor_pool/importer instance should outlive msg_factory, as msg_factory caches prototypes */
-  std::unique_ptr<google::protobuf::DynamicMessageFactory> msg_factory = nullptr;
-
-  google::protobuf::FileDescriptorProto file_descriptor_proto;
-  google::protobuf::DescriptorProto *descriptor_proto = nullptr;
-
-  int field_id = 1;
 };
 
 class LogMessageProtobufFormatter
