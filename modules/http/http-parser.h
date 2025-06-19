b317a1808aec76892a71308688647ef097ffe883
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-06-19 11:39:10 +0200

    modules: change license to GPL3+
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- modules/http/http-parser.h
+++ modules/http/http-parser.h
@@ -1,9 +1,10 @@
 /*
  * Copyright (c) 2016 Marc Falzon
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * As an additional exemption you are allowed to compile & link against the
  * OpenSSL libraries as published by the OpenSSL project. See the file
