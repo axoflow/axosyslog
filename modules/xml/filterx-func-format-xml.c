efe924aa8ada8e75f60dfa4ed53d06c8761d5e0f
Tamás Kosztyu <tamas.kosztyu@axoflow.com>
2025-06-20 11:11:19 +0200

    xml: add leaf appending to format_xml
    Signed-off-by: Tamás Kosztyu <tamas.kosztyu@axoflow.com>

--- modules/xml/filterx-func-format-xml.c
+++ modules/xml/filterx-func-format-xml.c
@@ -21,11 +21,89 @@
  *
  */
 #include "filterx-func-format-xml.h"
+#include "filterx/object-extractor.h"
+#include "filterx/filterx-eval.h"
+#include "scratch-buffers.h"
+#include "filterx/object-dict-interface.h"
+
+#define FILTERX_FUNC_FORMAT_XML_USAGE "Usage: format_xml([dict])"
+const char *XML_ERROR_STR = "Failed to convert to xml";
+
+static void
+_append_leaf(const char *key_str, const char *value_str, gsize value_str_len, GString *buffer)
+{
+  if(value_str_len)
+    {
+      g_string_append_printf(buffer, "<%s>%s</%s>", key_str, value_str, key_str);
+    }
+  else
+    {
+      g_string_append_printf(buffer, "<%s/>", key_str);
+    }
+}
+
+static gboolean
+_append_entry(FilterXObject *key, FilterXObject *value, gpointer user_data)
+{
+  GString *buffer = ((gpointer *) user_data)[1];
+  const gchar *key_str;
+  gsize key_str_len;
+
+  (void)filterx_object_extract_string_ref(key, &key_str, &key_str_len);
+
+  GString *val_buf = scratch_buffers_alloc();
+  if (!filterx_object_str(value, val_buf))
+    {
+      FilterXFunctionFormatXML *self = ((gpointer *) user_data)[0];
+      gchar type_name_buf[FILTERX_OBJECT_TYPE_NAME_BUF_SIZE];
+      gchar *info = g_strdup_printf("Couldn't convert value to string, type: %s",
+                                    filterx_object_format_type_name(value, type_name_buf));
+      filterx_eval_push_error_info(XML_ERROR_STR, &self->super.super, info, TRUE);
+      return FALSE;
+    }
+
+  append_leaf(key_str, val_buf->str, val_buf->len, buffer);
+  return TRUE;
+}
+
+static gboolean
+_append_to_buffer(FilterXObject *key, FilterXObject *value, gpointer user_data)
+{
+
+  if(!_append_entry(key, value, user_data))
+    return FALSE;
+
+  return TRUE;
+}
 
 static FilterXObject *
 _eval(FilterXExpr *s)
 {
-  return NULL;
+  FilterXFunctionFormatXML *self = (FilterXFunctionFormatXML *) s;
+
+  FilterXObject *input_dict = filterx_expr_eval_typed(self->input);
+  if (!input_dict)
+    return NULL;
+
+  GString *formatted = scratch_buffers_alloc();
+  FilterXObject *input_dict_unwrapped = filterx_ref_unwrap_ro(input_dict);
+  if (!filterx_object_is_type(input_dict_unwrapped, &FILTERX_TYPE_NAME(dict)))
+    {
+      filterx_object_unref(input_dict);
+      filterx_eval_push_error("input must be a dict. " FILTERX_FUNC_FORMAT_XML_USAGE, s, input_dict);
+      return NULL;
+    }
+
+  gpointer user_data[] = { self, formatted };
+
+  if (!filterx_dict_iter(input_dict_unwrapped, append_to_buffer, user_data))
+    {
+      filterx_object_unref(input_dict);
+      return NULL;
+    }
+
+  filterx_object_unref(input_dict);
+  return filterx_string_new(formatted->str, formatted->len);
 }
 
 static FilterXExpr *
