84e4bf0f6fd6d874c1a6bff2aac5ee38172ee271
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-09-21 14:35:15 +0200

    grpc: use a single last_grpc_{client,server}_credentials_builder variable
    In case we are linking modules into the same executable.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

---
+++ modules/grpc/common/grpc-grammar-internal.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2025 Balazs Scheidler <balazs.scheidler@axoflow.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+#include "grpc-grammar-internal.h"
+#include "credentials/grpc-credentials-builder.h"
+
+GrpcServerCredentialsBuilderW *last_grpc_server_credentials_builder;
+GrpcClientCredentialsBuilderW *last_grpc_client_credentials_builder;
