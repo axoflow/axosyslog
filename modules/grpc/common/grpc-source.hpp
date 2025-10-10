34ffd1569e76a2b85957ce689707f1af85c5fffc
László Várady <laszlo.varady@anno.io>
2025-10-10 16:25:38 +0200

    grpc: remove request_exit() interface from GrpcSourceDriver
    It belongs to the worker.
    
    Signed-off-by: László Várady <laszlo.varady@anno.io>

--- modules/grpc/common/grpc-source.hpp
+++ modules/grpc/common/grpc-source.hpp
@@ -49,7 +49,6 @@ public:
 
   virtual gboolean init();
   virtual gboolean deinit();
-  virtual void request_exit() = 0;
   virtual void format_stats_key(StatsClusterKeyBuilder *kb) = 0;
   virtual const char *generate_persist_name() = 0;
   virtual LogThreadedSourceWorker *construct_worker(int worker_index) = 0;
