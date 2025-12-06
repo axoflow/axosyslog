6400392afc848666b4572e807b3dccd209099269
Attila Szakacs <attila.szakacs@axoflow.com>
2025-12-06 14:28:57 +0100

    logqueue-disk-non-reliable: fix leak in _pop_from_memory_queue_head()
    We steal the ref.
    
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

--- modules/diskq/logqueue-disk-non-reliable.c
+++ modules/diskq/logqueue-disk-non-reliable.c
@@ -87,7 +87,7 @@ _pop_node_from_memory_queue_head(LogQueueDiskMemoryQueue *memory_queue, LogMessa
 static void
 _extract_queue_node(LogMessageQueueNode *node, LogMessage **pmsg, LogPathOptions *path_options)
 {
-  *pmsg = log_msg_ref(node->msg);
+  *pmsg = node->msg;
   path_options->ack_needed = node->ack_needed;
 }
 
