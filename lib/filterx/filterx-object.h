35fa8b7b44d673a6c84f968cc963a6d8a9c5e2b4
Balazs Scheidler <balazs.scheidler@axoflow.com>
2026-02-09 16:08:40 +0100

    filterx: use filterx_object_vref/vunref() when we know the value can't be NULL
    This is not an exhaustive list, this only makes any sense in hotpaths though.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

--- lib/filterx/filterx-object.h
+++ lib/filterx/filterx-object.h
@@ -422,7 +422,7 @@ filterx_object_unmarshal(FilterXObject *self)
 {
   if (self->type->unmarshal)
     return self->type->unmarshal(self);
-  return filterx_object_ref(self);
+  return filterx_object_vref(self);
 }
 
 static inline gboolean
@@ -760,7 +760,7 @@ static inline FilterXObject *
 filterx_object_copy(FilterXObject *self)
 {
   if (!self->type->is_mutable)
-    return filterx_object_ref(self);
+    return filterx_object_vref(self);
 
   if (self->floating_ref)
     {
@@ -854,7 +854,7 @@ filterx_object_cow_fork2(FilterXObject *self, FilterXObject **pself)
         return filterx_ref_float(self);
 
       FilterXObject *result = filterx_ref_float(filterx_object_copy(self));
-      filterx_object_unref(self);
+      filterx_object_vunref(self);
       return result;
     }
 }
