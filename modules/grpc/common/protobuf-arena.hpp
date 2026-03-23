f4e2ab9770bd332ac98182b2055abc730bd587f2
Szilard Parrag <szilard.parrag@axoflow.com>
2026-03-23 10:04:47 +0100

    otel-dest: create compat for grpc arena
    Before version 26 `Arena::CreateMessage` MUST be used.
    Starting with version 26 it is possible to use the unified `Arena::Create` method.
    
    It is unfortunately not possible to use `Arena::Create` on all versions
    as the neccessary changes made in it are only available starting with v26.0.
    Prior revisions do not properly populate arena memory if used through `Arena::Create`.
    
    Signed-off-by: Szilard Parrag <szilard.parrag@axoflow.com>

---
+++ modules/grpc/common/protobuf-arena.hpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2026 Axoflow
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef PROTOBUF_ARENA_HPP
+#define PROTOBUF_ARENA_HPP
+
+#include <google/protobuf/arena.h>
+#include <google/protobuf/stubs/common.h>
+
+namespace syslogng {
+namespace grpc {
+
+template <typename T>
+T *arena_create_message(google::protobuf::Arena *arena)
+{
+#if GOOGLE_PROTOBUF_VERSION >= 5026000
+  return google::protobuf::Arena::Create<T>(arena);
+#else
+  return google::protobuf::Arena::CreateMessage<T>(arena);
+#endif
+}
+
+}
+}
+
+#endif
