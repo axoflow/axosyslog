44422b830de0462c8912ad71bd5d80f74ff6eae2
László Várady <laszlo.varady@anno.io>
2026-08-14 13:55:36 +0200

    ehttp: implement source
    Signed-off-by: László Várady <laszlo.varady@anno.io>

--- modules/http/source/http-source.c
+++ modules/http/source/http-source.c
@@ -22,9 +22,160 @@
  */
 
 #include "http-source.h"
+#include "http/http-message.h"
+#include "http/http-message-internal.h"
 #include "socket/socket-options-inet.h"
+#include "logmsg/logmsg.h"
+#include "msg-format.h"
 
+#include <string.h>
 #include <strings.h>
+
+#include <json.h>
+
+typedef GQueue *(*EHTTPExtractMessageFunc)(HTTPRequest *http_request, HTTPSourceConnection *connection);
+
+GQueue *
+_extract_single_message(HTTPRequest *http_request, HTTPSourceConnection *connection)
+{
+  EHTTPSourceDriver *self = (EHTTPSourceDriver *) connection->owner;
+  MsgFormatOptions *parse_options = &self->super.super.reader_options.parse_options;
+
+  const GByteArray *body = http_message_get_body(&http_request->super);
+  if (!body || !body->data)
+    return NULL;
+
+  GQueue *messages = g_queue_new();
+
+  const gchar *message = (const gchar *) body->data;
+  LogMessage *msg = msg_format_construct_message(parse_options, (guchar *) message, body->len);
+  gsize message_length = body->len;
+  msg_format_parse_into(parse_options, msg, (guchar *) message, &message_length);
+
+  g_queue_push_tail(messages, msg);
+
+  return messages;
+}
+
+GQueue *
+_extract_messages_line_separated(HTTPRequest *http_request, HTTPSourceConnection *connection)
+{
+  EHTTPSourceDriver *self = (EHTTPSourceDriver *) connection->owner;
+  MsgFormatOptions *parse_options = &self->super.super.reader_options.parse_options;
+
+  http_request_null_terminate_body(http_request);
+  const GByteArray *body = http_message_get_body(&http_request->super);
+  if (!body || !body->data)
+    return NULL;
+
+  GQueue *messages = g_queue_new();
+
+  gchar *state;
+  gchar *data = (gchar *) body->data;
+  gchar *line = strtok_r(data, "\n", &state);
+
+  while (line)
+    {
+      gsize line_length = strlen(line);
+      LogMessage *msg = msg_format_construct_message(parse_options, (guchar *) line, line_length);
+      msg_format_parse_into(parse_options, msg, (guchar *) line, &line_length);
+      g_queue_push_tail(messages, msg);
+
+      line = strtok_r(NULL, "\n", &state);
+    }
+  return messages;
+}
+
+static GQueue *
+_extract_from_json(struct json_object *obj, GSockAddr *saddr, MsgFormatOptions *parse_options)
+{
+  struct json_object *messages_obj = obj;
+
+  if (!json_object_is_type(obj, json_type_array))
+    {
+      struct json_object_iter itr;
+      json_object_object_foreachC(obj, itr)
+      {
+        if (json_object_is_type(itr.val, json_type_array))
+          {
+            messages_obj = itr.val;
+            break;
+          }
+      }
+
+      if (messages_obj == obj)
+        {
+          msg_warning("Error extracting JSON messages, array object is not found");
+          return NULL;
+        }
+    }
+
+  GQueue *messages = g_queue_new();
+
+  gsize messages_size = json_object_array_length(messages_obj);
+  for (gsize i = 0; i < messages_size; ++i)
+    {
+      struct json_object *message_obj = json_object_array_get_idx(messages_obj, i);
+      const gchar *message = json_object_get_string(message_obj);
+
+      gsize message_length = strlen(message);
+      LogMessage *msg = msg_format_construct_message(parse_options, (guchar *) message, message_length);
+      msg_format_parse_into(parse_options, msg, (guchar *) message, &message_length);
+      g_queue_push_tail(messages, msg);
+    }
+
+  return messages;
+}
+
+GQueue *
+_extract_messages_json_array(HTTPRequest *http_request, HTTPSourceConnection *connection)
+{
+  EHTTPSourceDriver *self = (EHTTPSourceDriver *) connection->owner;
+  MsgFormatOptions *parse_option = &self->super.super.reader_options.parse_options;
+
+  const GByteArray *body = http_message_get_body(&http_request->super);
+  if (!body || !body->data)
+    return NULL;
+
+  struct json_tokener *tok = json_tokener_new();
+  struct json_object *obj = json_tokener_parse_ex(tok, (const gchar *) body->data, body->len);
+  if (tok->err != json_tokener_success || !obj)
+    {
+      msg_warning("Error parsing JSON messages");
+      json_tokener_free(tok);
+      return NULL;
+    }
+  json_tokener_free(tok);
+
+  GQueue *messages = _extract_from_json(obj, connection->peer_addr, parse_option);
+  json_object_put(obj);
+
+  return messages;
+}
+
+/* EHTTPSourceMode ordering */
+static EHTTPExtractMessageFunc ehttp_extract_modes[] =
+{
+  _extract_single_message,
+  _extract_messages_line_separated,
+  _extract_messages_json_array
+};
+
+HTTPResponse *
+_create_response(HTTPRequest *http_request, HTTPSourceConnection *connection)
+{
+  HTTPResponse *http_response = http_response_new_empty();
+  http_message_set_http_version(&http_response->super, 1, 1);
+  http_response_set_status_code(http_response, HTTP_OK);
+
+  GByteArray* body = g_byte_array_sized_new(32);
+  const gchar *status_line = http_response_status_code_to_status_line(HTTP_OK);
+  g_byte_array_append(body, (const guint8 *) status_line, strlen(status_line));
+
+  http_message_take_body(&http_response->super, body);
+
+  return http_response;
+}
 
 gboolean
 ehttp_sd_set_mode(LogDriver *d, const gchar *mode)
@@ -43,6 +194,16 @@ ehttp_sd_set_mode(LogDriver *d, const gchar *mode)
   return TRUE;
 }
 
+gboolean
+ehttp_sd_init(LogPipe *s)
+{
+  EHTTPSourceDriver *self = (EHTTPSourceDriver *) s;
+
+  self->super.extract_log_messages = ehttp_extract_modes[self->mode];
+
+  return http_sd_init_method(s);
+}
+
 EHTTPSourceDriver *
 ehttp_sd_new(GlobalConfig *cfg)
 {
@@ -50,6 +211,9 @@ ehttp_sd_new(GlobalConfig *cfg)
   http_sd_init_instance(&self->super, socket_options_inet_new(), http_transport_mapper_new(), cfg);
 
   self->mode = EHTTP_SINGLE;
+
+  self->super.create_response = _create_response;
+  self->super.super.super.super.super.init = ehttp_sd_init;
 
   return self;
 }
